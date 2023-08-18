f32373778bc39ea5b494b711373bba9ab63b2dcb
Abby Jackson <ajac@softchoice.com>
2023-08-17 23:45:13 -0400

    Completed Module9 Challenge

---
+++ M9-Challenge-Jackson-Abigail/starter/record-secure-app/src/main/java/com/company/recordsecureapp/RecordSecureAppApplication.java
@@ -0,0 +1,16 @@
+package com.company.recordsecureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+public class RecordSecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordSecureAppApplication.class, args);
+	}
+
+}
---
+++ M9-Challenge-Jackson-Abigail/starter/record-secure-app/src/test/java/com/company/recordsecureapp/RecordSecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.recordsecureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordSecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
